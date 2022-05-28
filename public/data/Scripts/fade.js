fce949d6702fa434eacd7ea720201a2b6e92b8fb
Abos2024 <abos2024@gmail.com>
2022-05-28 19:02:25 +0300

    Update fade.js

--- public/data/Scripts/fade.js
+++ public/data/Scripts/fade.js
@@ -1,16 +1,16 @@
 import Highway from '..\..\node_modules\@dogstudio\highway';
 import Tween from'..\..\node_modules\gsap';
-
 class Fade extends Highway.Transition{
     in({from,to,done}){
-        // Remove the old content
+       // Remove the old content
         from.remove();
         // Animation
-        Tween.fromto(to,0.5,{opacity:0},{opacity:1,onComplete:done})
+        Tween.fromto(to,0.5,{opacity:0},{opacity:1,onComplete:done});
    }
     out({from,done}){
         // Animation
-        done();
+        Tween.fromto(from,0.5,{opacity:1},{opacity:0,onComplete:done});
    }
-  // Export Fade class
-  export default Fade;
+}
+// Export Fade class
+export default Fade;
