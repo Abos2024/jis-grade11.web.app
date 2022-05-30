e3c5cabfc8d55f2d0d91d9f45567bf337d8614e9
Abos2024 <abos2024@gmail.com>
2022-05-30 14:13:44 +0300

    Tweaks

--- public/data/Scripts/script.js
+++ public/data/Scripts/script.js
@@ -1,3 +1,39 @@
+const toggle1 = document.getElementById('toggle-theme');
+toggle1.onclick = function() {togglee()};
+function togglee(){  
+if (toggle1.innerHTML === 'toggle_on'){
+  toggle1.innerHTML = 'toggle_off';
+  toggle1.style.fontVariationSettings = "'FILL' 0"
+} else if (toggle1.innerHTML = 'toggle_on'){
+  toggle1.innerHTML = 'toggle_on';
+  toggle1.style.fontVariationSettings = "'FILL' 1"
+} else{
+  toggle1.innerHTML = 'error';
+  toggle1.style.fontVariationSettings = "'FILL' 1"
+}};
+
+const toggle2 = document.getElementById('toggle-grad');
+toggle2.onclick = function() {togglee2()};
+function togglee2(){  
+if (toggle2.innerHTML === 'toggle_on'){
+  toggle2.innerHTML = 'toggle_off';
+  toggle2.style.fontVariationSettings = "'FILL' 0"
+} else if (toggle2.innerHTML = 'toggle_on'){
+  toggle2.innerHTML = 'toggle_on';
+  toggle2.style.fontVariationSettings = "'FILL' 1"
+} else{
+  toggle2.innerHTML = 'error';
+  toggle2.style.fontVariationSettings = "'FILL' 1"
+}};
+
+var changeclrbtn = document.getElementById('main-colorbtn');
+
+changeclrbtn.onclick = function() {maincolorset()};
+function maincolorset() {
+  var maincolore = document.getElementById('main-colorbox').value;
+  document.documentElement.style.setProperty('--main-color', maincolore );
+}
+
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
