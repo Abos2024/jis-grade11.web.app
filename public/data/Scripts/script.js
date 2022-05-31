d6557acabeb7bcc84f6b62aa7f712c1909548e29
Abos2024 <abos2024@gmail.com>
2022-05-31 10:46:41 +0300

    Fixed an issue

--- public/data/Scripts/script.js
+++ public/data/Scripts/script.js
@@ -1,45 +1,5 @@
-const toggle1 = document.getElementById('toggle-theme');
-toggle1.onclick = function() {togglee()};
-function togglee(){  
-if (toggle1.innerHTML === 'toggle_on'){
-  toggle1.innerHTML = 'toggle_off';
-  toggle1.style.fontVariationSettings = "'FILL' 0"
-} else if (toggle1.innerHTML = 'toggle_on'){
-  toggle1.innerHTML = 'toggle_on';
-  toggle1.style.fontVariationSettings = "'FILL' 1"
-} else{
-  toggle1.innerHTML = 'error';
-  toggle1.style.fontVariationSettings = "'FILL' 1"
-}};
 
-const toggle2 = document.getElementById('toggle-grad');
-toggle2.onclick = function() {togglee2()};
-function togglee2(){  
-if (toggle2.innerHTML === 'toggle_on'){
-  toggle2.innerHTML = 'toggle_off';
-  toggle2.style.fontVariationSettings = "'FILL' 0"
-} else if (toggle2.innerHTML = 'toggle_on'){
-  toggle2.innerHTML = 'toggle_on';
-  toggle2.style.fontVariationSettings = "'FILL' 1"
-} else{
-  toggle2.innerHTML = 'error';
-  toggle2.style.fontVariationSettings = "'FILL' 1"
-}};
 
-var changeclrbtn = document.getElementById('main-colorbtn');
-
-changeclrbtn.onclick = function() {maincolorset()};
-function maincolorset() {
-  var maincolore = document.getElementById('main-colorbox').value;
-  document.documentElement.style.setProperty('--main-color', maincolore );
-  var currentTheme = localStorage.getItem("color-theme");
-if (currentTheme != maincolore) {
-  document.documentElement.style.setProperty('--main-color', maincolore );
-} else if (currentTheme == maincolore) {
-  document.documentElement.style.setProperty('--main-color', '#499ae8' );
-}
-  localStorage.setItem("color-theme", maincolore);
-}
 function openNav() {
   document.getElementById("myNav").style.width = "100%";
 }
@@ -85,6 +45,44 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active-slide";
+}
+
+const toggle1 = document.getElementById('toggle-theme');
+toggle1.onclick = function() {togglee()};
+function togglee(){  
+if (toggle1.innerHTML === 'toggle_on'){
+  toggle1.innerHTML = 'toggle_off';
+  toggle1.style.fontVariationSettings = "'FILL' 0"
+} else if (toggle1.innerHTML = 'toggle_on'){
+  toggle1.innerHTML = 'toggle_on';
+  toggle1.style.fontVariationSettings = "'FILL' 1"
+} else{
+  toggle1.innerHTML = 'error';
+  toggle1.style.fontVariationSettings = "'FILL' 1"
+}};
+
+const toggle2 = document.getElementById('toggle-grad');
+toggle2.onclick = function() {togglee2()};
+function togglee2(){  
+if (toggle2.innerHTML === 'toggle_on'){
+  toggle2.innerHTML = 'toggle_off';
+  toggle2.style.fontVariationSettings = "'FILL' 0"
+} else if (toggle2.innerHTML = 'toggle_on'){
+  toggle2.innerHTML = 'toggle_on';
+  toggle2.style.fontVariationSettings = "'FILL' 1"
+} else{
+  toggle2.innerHTML = 'error';
+  toggle2.style.fontVariationSettings = "'FILL' 1"
+}};
+
+const changeclrbtn = document.getElementById('main-colorbtn');
+
+changeclrbtn.onclick = function() {maincolorset()};
+function maincolorset() {
+  var maincolore = document.getElementById('main-colorbox').value;
+  window.localStorage.setItem('color-theme', maincolore);
+  var currentTheme = localStorage.getItem('color-theme');
+  document.documentElement.style.setProperty('--main-color', currentTheme );
 }
 
 
